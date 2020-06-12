65bca8b00e2678d67134e31fe701f2296d27a3ff
Alexander Valai <valexxjr@gmail.com>
2020-06-12 17:40:30 +0300

    added first contacts view

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Contact
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    contact_list = Contact.objects.all()
+    template = loader.get_template('crud/contact_table.html')
+    context = {
+        'contact_list': contact_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, contact_id):
+    return HttpResponse("You're looking at contact %s." % contact_id)
+
