9f5975019331a4001ac9d317c9ab1d3cd0b91aec
Alexander Valai <valexxjr@gmail.com>
2020-06-15 14:58:57 +0300

    used namespace for url href

--- crud/urls.py
+++ crud/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'crud'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacts/<int:contact_id>/', views.detail, name='detail')
