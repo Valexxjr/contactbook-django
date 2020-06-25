e2f76330cf4d7565c7a055cd6d5a8771ac74e0aa
Alexander Valai <valexxjr@gmail.com>
2020-06-25 04:29:53 +0300

    added views for creation, updating and deleting objects

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
 from . import views
+from .views import delete_contact, delete_phone, delete_attachment
 
 app_name = 'crud'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/update', views.ContactUpdate.as_view(), name='update'),
+    path('contacts/<int:pk>/', views.DetailView.as_view(), name='contact_detail'),
+    path('contacts/<int:pk>/update', views.ContactUpdate.as_view(), name='contact_update'),
+    path('contacts/<int:pk>/delete', delete_contact, name='contact_delete'),
+    path('contacts/<int:contact_id>/phones/<int:pk>/update', views.PhoneUpdate.as_view(), name='phone_update'),
+    path('contacts/<int:contact_id>/attachments/<int:pk>/update', views.AttachmentUpdate.as_view(), name='attachment_update'),
+    path('contacts/<int:pk>/phones/create', views.PhoneCreate.as_view(), name='phone_create'),
+    path('contacts/<int:pk>/attachments/create', views.AttachmentCreate.as_view(), name='attachment_create'),
+    path('contacts/<int:contact_id>/phones/<int:pk>/delete', delete_phone, name='phone_delete'),
+    path('contacts/<int:contact_id>/attachments/<int:pk>/delete', delete_attachment, name='attachment_delete'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import generic
 
-from .models import Contact
+from .models import Contact, Phone, Attachment
 
 
 class IndexView(generic.ListView):
@@ -14,7 +17,70 @@ class DetailView(generic.DetailView):
 
 class ContactUpdate(generic.UpdateView):
     model = Contact
-    fields = ['first_name', 'last_name', 'patronymic', 'birth_date', 'city']
+    fields = ['first_name', 'last_name', 'patronymic', 'birth_date', 'city', 'country', 'citizenship']
     template_name_suffix = '_form'
 
-    success_url = '/crud'
+    def get_success_url(self):
+        return reverse('crud:contact_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+
+class PhoneUpdate(generic.UpdateView):
+    model = Phone
+    fields = ['phone_number', 'operator_code', 'country_code', 'note']
+    template_name_suffix = '_form'
+
+    def get_success_url(self):
+        return reverse('crud:contact_detail', kwargs={'pk': self.kwargs.get('contact_id')})
+
+
+class PhoneCreate(generic.CreateView):
+    model = Phone
+    fields = ['phone_number', 'operator_code', 'country_code', 'note']
+    template_name_suffix = '_form'
+
+    def form_valid(self, form):
+        form.instance.contact = Contact.objects.get(id=self.kwargs.get('pk'))
+        return super(PhoneCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('crud:contact_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+
+class AttachmentUpdate(generic.UpdateView):
+    model = Attachment
+    fields = ['file_name', 'upload_date', 'note']
+    template_name_suffix = '_form'
+
+    def get_success_url(self):
+        return reverse('crud:contact_detail', kwargs={'pk': self.kwargs.get('contact_id')})
+
+
+class AttachmentCreate(generic.CreateView):
+    model = Attachment
+    fields = ['file_name', 'upload_date', 'note']
+    template_name_suffix = '_form'
+
+    def form_valid(self, form):
+        form.instance.contact = Contact.objects.get(id=self.kwargs.get('pk'))
+        return super(AttachmentCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('crud:contact_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+
+def delete_contact(request, pk):
+    contact = Contact.objects.get(pk=pk)
+    contact.delete()
+    return redirect('crud:index')
+
+
+def delete_phone(request, contact_id, pk):
+    phone = Phone.objects.get(pk=pk)
+    phone.delete()
+    return HttpResponseRedirect(reverse('crud:contact_detail', args=(contact_id,)))
+
+
+def delete_attachment(request, contact_id, pk):
+    attachment = Attachment.objects.get(pk=pk)
+    attachment.delete()
+    return HttpResponseRedirect(reverse('crud:contact_detail', args=(contact_id,)))
